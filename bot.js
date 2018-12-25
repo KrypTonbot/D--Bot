2f911eba51b24aea16366c792ba3c96d41b287fa
KrypTonbot <45620388+KrypTonbot@users.noreply.github.com>
2018-12-25 10:28:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,14 +31,14 @@ client.on("ready", () => { // كود رينبو
   setInterval(lol, 5000);
 });
 
-bot.editChannel("527026652396650496", { name : "✲"});
-bot.editChannel("527026652396650496", { name : "✲W"});
-bot.editChannel("527026652396650496", { name : "✲We"});
-bot.editChannel("527026652396650496", { name : "✲Wel"});
-bot.editChannel("527026652396650496", { name : "✲Welc"});
-bot.editChannel("527026652396650496", { name : "✲Welco"});
-bot.editChannel("527026652396650496", { name : "✲Welcom"});
-bot.editChannel("527026652396650496", { name : "✲Welcome"});
-bot.editChannel("527026652396650496", { name : "✲Welcome✲"});
+bot.editChannel("527026652509634572", { name : "✲"});
+bot.editChannel("527026652509634572", { name : "✲W"});
+bot.editChannel("527026652509634572", { name : "✲We"});
+bot.editChannel("527026652509634572", { name : "✲Wel"});
+bot.editChannel("527026652509634572", { name : "✲Welc"});
+bot.editChannel("527026652509634572", { name : "✲Welco"});
+bot.editChannel("527026652509634572", { name : "✲Welcom"});
+bot.editChannel("527026652509634572", { name : "✲Welcome"});
+bot.editChannel("527026652509634572", { name : "✲Welcome✲"});
 
 client.login('NTI3MDEyODk3MjAxNTg2MTkx.DwNizw.NfSNkQzlaKZPXQZWQ1V1oVuQTDI'); 
