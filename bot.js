af385662641d5eb9b630391e0dbcce93dac63fbc
KrypTonbot <45620388+KrypTonbot@users.noreply.github.com>
2018-12-25 10:36:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// playing
-client.on('ready', () => {
-client.user.setGame(`,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(-help`,'https://www.twitch.tv/v5bz%27);
-});
-
 client.on('message', message => {
   let anything = [
     "وعليكم السلام",
