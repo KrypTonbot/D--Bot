4d6936c99f12650c1ffd29597536b2db9601513a
KrypTonbot <45620388+KrypTonbot@users.noreply.github.com>
2018-12-25 10:53:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,4 +63,33 @@ if(msg.content.startsWith(prefix + "tag")){ // !tag @someone
 
 })
 
+client.on('message', message => {
+  let anything = [
+    ":no_entry_sign: 《1》ممنوع السب او الشتم 
+:no_entry_sign: 《2》ممنوع السبام 
+:no_entry_sign: 《3》ممنوع الأعلان عن اي سيرفر 
+:no_entry_sign: 《4》ممنوع نشر روابط والباتشات 
+:no_entry_sign: 《5》ممنوع أستخدام مغير صوت 
+:no_entry_sign: 《6》ممنوع استخدام البوتات الا في المكان المخصص لها
+:no_entry_sign: 《7》ممنوع طلب الرتب من  الادارة لاي سبب
+:no_entry_sign: 《7》ممنوع كتابة الاوامر في روم الغير مخصص لها
+:no_entry_sign: 《8》ممنوع استغلال الأخطاء او الثغرات 
+:no_entry_sign: 《9》يمنع ازعاج الاعضاء
+:no_entry_sign: 《10》اذا احد ازعجك او خالف القوانين اخبر الاونر او الادمن
+
+(:point_left: ونتمنا منكم تعطونا بعض النصايح لمستقبل السيرفر في روم النصايح :point_right: )
+
+
+:eight_pointed_black_star: أي اسفسار كلم الاداره :eight_pointed_black_star:
+"
+    
+    
+  ]
+  if(message.content === '#rules'){
+    message.reply(`${anything[Math.floor(Math.random() * anything.length)]}`)
+    
+    return;
+  }
+});
+
 client.login('NTI3MDEyODk3MjAxNTg2MTkx.DwNizw.NfSNkQzlaKZPXQZWQ1V1oVuQTDI'); 
