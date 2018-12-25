0a7900847eae466db26aceddc3e94d1d636532cb
KrypTonbot <45620388+KrypTonbot@users.noreply.github.com>
2018-12-25 11:10:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,5 +75,22 @@ client.on('message',async msg => {//Toxic Codes
  
 });
 
+client.on('message', message => {
+    if (message.content === ('#bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `[<@352533519068823556>]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
 
 client.login('NTI3MDEyODk3MjAxNTg2MTkx.DwOEkw.4EuRdJvabNSox6HJw60W7_bxcHM'); 
